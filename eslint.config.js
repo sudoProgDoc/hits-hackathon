c8ab2dc862d46cd984984f432010ea19dd6cdd77
Kris <sudoprogdoc@gmail.com>
2025-06-16 21:46:38 +0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,38 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  extends: 'next/core-web-vitals',
-  rules: {
-    'eqeqeq': ['error', 'always'],
-    'no-console': 'warn',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': 'warn',
-    'jsx-a11y/anchor-is-valid': 'warn',
-    'import/prefer-default-export': 'off',
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+/** @type {import('eslint').Linter.FlatConfig} */
+const config = [
+  {
+    ignores: ['node_modules/**', 'build/**'],
   },
-  settings: {
-    react: {
-      version: 'detect',
+  {
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        React: 'readonly',
+      },
+    },
+    rules: {
+      'eqeqeq': ['error', 'always'],
+      'no-console': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'import/prefer-default-export': 'off',
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+    },
+    plugins: {
+      react: 'plugin:react/recommended',
+      'jsx-a11y': 'plugin:jsx-a11y/recommended',
+      'import': 'plugin:import/errors',
     },
   },
-};
+];
 
-module.exports = config;
+export default config;
